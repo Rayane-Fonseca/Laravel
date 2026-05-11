2c6008d744e5b4f3179114fe3c9ac0067bca04b6
Rayane Porto de Jesus Fonseca <rayane15526@gmail.com>
2026-05-11 13:48:37 -0300

    Add files via upload

---
+++ storage/framework/views/a53e19a121cef652dc3e0674eafd3519.php
@@ -0,0 +1,4 @@
+<svg fill="none" stroke="currentColor" viewBox="0 0 24 24" <?php echo e($attributes); ?>>
+    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path>
+</svg>
+<?php /**PATH C:\Users\RES0123517\OneDrive\ProfThais\Desenvolvimentos_de_Sistemas\SAEP\E-commerce\ecommerce\vendor\laravel\framework\src\Illuminate\Foundation\Providers/../resources/exceptions/renderer/components/icons/check.blade.php ENDPATH**/ ?>
